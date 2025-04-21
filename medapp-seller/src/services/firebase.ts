5d62442a415505a6050aa5634bf0f0e707c953f2
RASEEN Shahil <85159094+raseen-shahil@users.noreply.github.com>
2025-04-21 13:28:16 +0530

    Update firebase.ts

--- medapp-seller/src/services/firebase.ts
+++ medapp-seller/src/services/firebase.ts
@@ -3,15 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage, ref } from 'firebase/storage';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyACyWch0LkThsdp6HWmm9qrKJmmiupxA9M",
-    authDomain: "medapp-38667.firebaseapp.com",
-    projectId: "medapp-38667",
-    storageBucket: "medapp-38667.firebasestorage.app",
-    messagingSenderId: "207069580230",
-    appId: "1:207069580230:web:e02dd0e16e913831cfc0c7",
-    measurementId: "G-6NKLW8Y7DQ"
-};
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
