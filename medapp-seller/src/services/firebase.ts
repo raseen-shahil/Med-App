3f99a1f2f194db9f632e2945854fba738641cac8
RASEEN Shahil <85159094+raseen-shahil@users.noreply.github.com>
2025-05-19 11:45:42 +0530

    Update firebase.ts

--- medapp-seller/src/services/firebase.ts
+++ medapp-seller/src/services/firebase.ts
@@ -3,15 +3,6 @@ import { getAuth, setPersistence, browserLocalPersistence, connectAuthEmulator }
 import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
 import { getStorage, ref } from 'firebase/storage';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyACyWch0LkThsdp6HWmm9qrKJmmiupxA9M",
-    authDomain: "medapp-38667.firebaseapp.com",
-    projectId: "medapp-38667",
-    storageBucket: "medapp-38667.firebasestorage.app",
-    messagingSenderId: "207069580230",
-    appId: "1:207069580230:web:e02dd0e16e913831cfc0c7",
-    measurementId: "G-6NKLW8Y7DQ"
-};
 
 // Initialize Firebase only if not already initialized
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
